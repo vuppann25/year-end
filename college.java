7a961bb7a90fcaefe02e1a2da334ac0f7106ad6f
KrazyKaren123 <98198541+KrazyKaren123@users.noreply.github.com>
2025-06-09 00:17:18 -0700

    Update college.java
    put from VS to here since initial push and commits were not syncing

--- college.java
+++ college.java
@@ -1,3 +1,40 @@
 public class college {
-    
+    private String name;
+    private double acceptanceRate;
+
+    public college(String name, double acceptanceRate) {
+        this.name = name;
+        this.acceptanceRate = acceptanceRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAcceptanceRate() {
+        return acceptanceRate;
+    }
+
+    public void setAcceptanceRate(double acceptanceRate) {
+        this.acceptanceRate = acceptanceRate;
+    }
+
+    public String compareAcceptanceRate(double userChance) {
+        if (userChance < acceptanceRate) {
+            return "You have a good chance of being accepted into " + name + " (Acceptance Rate: " + acceptanceRate
+                    + "%).";
+        } else {
+            return "Your personal chance is lower than or equal to " + name + " (Acceptance Rate: " + acceptanceRate
+                    + "%). It might be a reach school.";
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "College: " + name + ", Acceptance Rate: " + acceptanceRate + "%";
+    }
 }
